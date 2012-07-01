b05a7616c4d03dbd0629e517f40187c2a1108bb8
Cameron Ross <elpenguino@gmail.com>
2012-07-01 04:06:55 -0300

    use ip address as seed

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ require_once 'Dwoo/dwooAutoload.php';
 $dwoo = new Dwoo();
 $chars = array('Terra', 'Locke', 'Celes', 'Relm', 'Strago', 'Cyan', 'Sabin', 'Mog', 'Edgar', 'Gogo', 'Shadow', 'Umaro', 'Setzer', 'Gau');
 
+if (!isset($_GET['trand']))
+        srand(ip2long($_SERVER['REMOTE_ADDR']));
+
 $finalchars = array();
 for ($i = 0; $i < 3; $i++) {
 	for ($j = 0; $j < 4; $j++) {
