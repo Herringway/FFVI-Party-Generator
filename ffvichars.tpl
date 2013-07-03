69e36f739f1f01d55a53217b19ddcc1fe6f353a8
Cameron Ross <elpenguino@gmail.com>
2013-07-03 15:02:56 -0300

    Enable support for more parties throughout the game

--- ffvichars.tpl
+++ ffvichars.tpl
@@ -5,6 +5,6 @@
 		<link rel="shortcut icon" href="ffvisprites/terra.gif" />
 	</head>
 	<body style="background: lightgray;">
-		<table>{loop $chars}<tr><td style="text-align: center; width: 100px; background: white;">Team {$_key+1}</td>{loop $}<td style="text-align: center; width: 75px; background: white;"><img alt="{$}" src="ffvisprites/{replace($, ' ', '%20')|lower}.gif"><br>{$}</td>{/loop}</tr>{/loop}</table>
+		{loop $parties}<table><tr><th colspan="5" style="text-align: center; background: white;">{$_key} {tif count($) > 1 ? "Parties" : "Party"}</th></tr>{loop $}<tr><td style="text-align: center; width: 100px; background: white;">Team {$_key+1}</td>{loop $}<td style="text-align: center; width: 75px; background: white;"><img alt="{$}" src="ffvisprites/{replace($, ' ', '%20')|lower}.gif"><br>{$}</td>{/loop}</tr>{/loop}</table>{/loop}
 	</body>
 </html>
--- index.php
+++ index.php
@@ -1,19 +1,61 @@
 <?php
 require_once 'Dwoo/dwooAutoload.php';
 $dwoo = new Dwoo();
-$chars = array('Terra', 'Locke', 'Celes', 'Relm', 'Strago', 'Cyan', 'Sabin', 'Mog', 'Edgar', 'Gogo', 'Shadow', 'Umaro', 'Setzer', 'Gau');
+$battleatnarshechars = array('Celes', 'Locke', 'Sabin', 'Edgar', 'Cyan', 'Gau', 'Terra');
+$postbattleatnarshechars = array('Celes', 'Locke', 'Sabin', 'Edgar', 'Cyan', 'Gau');
+$vectorchars = array('Shadow', 'Sabin', 'Edgar', 'Cyan', 'Gau'); $vectorreqs = array('Celes', 'Locke');
+$finalchars = array('Terra', 'Locke', 'Celes', 'Relm', 'Strago', 'Cyan', 'Sabin', 'Mog', 'Edgar', 'Gogo', 'Shadow', 'Umaro', 'Setzer', 'Gau');
 
 if (!isset($_GET['trand']))
         srand(ip2long($_SERVER['REMOTE_ADDR']));
 
-$finalchars = array();
-for ($i = 0; $i < 3; $i++) {
-	for ($j = 0; $j < 4; $j++) {
-		$x = array_rand($chars);
-		$finalchars[$i][] = $chars[$x];
-		unset($chars[$x]);
-	}
-}
+$parties = array();
+$parties['Battle at Narshe'] = generate_parties(3, $battleatnarshechars);
+$parties['To Zozo'] = generate_parties(1, $postbattleatnarshechars);
+$parties['Vector'] = generate_parties(1, $vectorchars, $vectorreqs);
+//$parties['Floating Continent'] = generate_parties(1, $vectorchars, $vectorreqs);
+$parties['World of Ruin'] = generate_parties(1, $finalchars);
+$parties['Final Dungeon'] = generate_parties(3, $finalchars);
 
-$dwoo->output('ffvichars.tpl', array('chars' => $finalchars));
+$dwoo->output('ffvichars.tpl', array('parties' => $parties));
+
+function generate_parties($numparties, $characters, $requiredchars = array()) {
+	$output = array();
+	
+	for ($i = 0; $i < $numparties; $i++)
+		$output[$i][] = get_and_remove_element($requiredchars, $characters);
+
+	$dist = random_distribution(min($numparties*3, count($characters)), 3, $numparties);
+
+	for ($i = 0; $i < $numparties; $i++)
+		for ($j = 0; $j < $dist[$i]; $j++)
+			$output[$i][] = get_and_remove_element($requiredchars, $characters);
+	
+	return $output;
+}
+function get_and_remove_element(&$arr1, &$arr2) {
+	$output = null;
+	if (count($arr1) > 0) {
+		$x = array_rand($arr1);
+		$output = $arr1[$x];
+		unset($arr1[$x]);
+	} else {
+		$x = array_rand($arr2);
+		$output = $arr2[$x];
+		unset($arr2[$x]);
+	}
+	return $output;
+}
+function random_distribution($range, $individuallimit, $divisions) {
+	if ($individuallimit * $divisions < $range)
+		throw new Exception("Cannot divide this range under these constraints");
+	$output = array();
+	for ($i = 0; $i < $divisions; $i++)
+		$output[$i] = 0;
+	for ($i = 0; $i < $range; $i++) {
+		for ($x = rand(0, $divisions-1); $output[$x] >= $individuallimit; $x = rand(0, $divisions-1));
+		$output[$x]++;
+	}
+	return $output;
+}
 ?>
